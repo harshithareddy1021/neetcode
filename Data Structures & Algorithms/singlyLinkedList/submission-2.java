e3dbe7f8b4abc0e30f004a960b7ec2ec6557831f
harshithareddy1021 <reddyharshitha370@gmail.com>
2026-07-19 17:14:36 +0530

    Add: singlyLinkedList - submission-2

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-2.java
@@ -0,0 +1,82 @@
+class LinkedList {
+    private Node head;
+    private int size;
+
+    private class Node{
+        Node next;
+        int val;
+
+        Node(int val){
+            this.val=val;
+        }
+    }
+
+    public LinkedList() {
+        head=null;
+        size=0;
+       
+    }
+
+    public int get(int index) {
+        if(index<0||index>=size)return -1;
+        Node temp=head;
+        while(index-->0){
+            temp=temp.next;
+        }
+        return temp.val;  
+    }
+
+
+    public void insertHead(int val) {
+        Node temp=new Node(val);
+        temp.next=head;
+        head=temp;
+        size++;
+        
+
+    }
+
+    public void insertTail(int val) {
+        if(head==null){
+            Node n=new Node(val);
+            head=n;
+            size++;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=new Node(val);
+        size++;
+
+    }
+
+    public boolean remove(int index) {
+        if(index<0||index>=size)return false;
+        if(index==0){
+            head=head.next;
+            size--;
+            return true;
+        }
+        Node temp=head;
+        while(index-->1){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+        size--;
+        return true;
+        
+    }
+
+    public ArrayList<Integer> getValues() {
+        ArrayList<Integer> l=new ArrayList<>();
+        Node temp=head;
+        while(temp!=null){
+            l.add(temp.val);
+            temp=temp.next;
+        }
+        return l;
+
+    }
+}
